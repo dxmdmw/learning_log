7f90aadebe5cfe4f5f714a62b4f2cb83fe4cf365
dxmdmw <duxiaoming1992@gmail.com>
2018-10-11 22:28:16 +0800

    Change some 500 to 404 erro pages.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -21,7 +21,8 @@ def topics(request):
 
 @login_required
 def topic(request, topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    '''显示单个主题及其所有的条目'''
+    topic = get_object_or_404(Topic, id=topic_id)
     # 确认请求的主题属于当前用户
     check_topic_owner(topic, request)
 
