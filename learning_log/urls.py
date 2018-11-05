be90af706a9045d8a2071d09671144957dc1864a
dxmdmw <duxiaoming1992@gmail.com>
2018-11-05 20:20:47 +0800

    optimize site navigation

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -33,10 +33,3 @@ urlpatterns = [
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# from django.contrib import admin
-# from django.conf.urls import include, url
-
-# urlpatterns = [
-#     url(r'^admin/', include(admin.site.urls))
-#     url(r'', include('learning_logs.urls',namespace='learning_logs')),
-# ]
